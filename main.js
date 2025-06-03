5d9199166ae6365ae4456868bf637e9f9eb25aba
luke89joseph <luke89joseph@gmail.com>
2025-06-02 21:32:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML = 
 `
-<a href="index.html"><img src="logo.png" id="logo"/></a>
-<a href="index.html" style="text-decoration: none;"><h3 style="color: rgb(65, 65, 151); font-weight: 800">BHS ROOM FINDER</h3></a>
+<a href="index.html" style="text-decoration: none;"><img src="logo.png" id="logo"/>
+<h3 style="color: rgb(65, 65, 151); font-weight: 800">BHS ROOM FINDER</h3></a>
 `;
 document.getElementById("space").innerHTML = 
 `
