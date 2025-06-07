763f6a17c16a3cf3a77db73e51aaa5b157155101
luke89joseph <luke89joseph@gmail.com>
2025-06-07 17:02:10 -0400

    Add files via upload

---
+++ roomfinder/main.js
@@ -0,0 +1,9 @@
+document.getElementById("header").innerHTML = 
+`
+<a href="index.html" style="text-decoration: none;"><img src="logo.png" id="logo"/>
+<h3 style="color: rgb(65, 65, 151); font-weight: 800">BHS ROOM FINDER</h3></a>
+`;
+document.getElementById("space").innerHTML = 
+`
+<h1></h1>
+`;
